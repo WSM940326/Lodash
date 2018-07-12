2156f8bf490cba28387b0a70cc6bf873e52c5328
WSM940326 <38807633+WSM940326@users.noreply.github.com>
2018-07-12 11:46:46 +0800

    0712

--- lodash/WSM940326-lodash.js
+++ lodash/WSM940326-lodash.js
@@ -1,10 +1,98 @@
-<script>
-var WSM940326 = {
-  chunk: function(array, size) {
 
-  }
+var WSM940326 = {
+// lodash --- 01:chunk
+  chunk: function (array, size = 1) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      result.push(array.slice(i, size+ 1))
+    }
+    return result
+  },
+
+
+// lodash --- 02:compact
+  compact: function (array) {
+    
+
+
+  },
+// lodash --- 03:concat
+
+  concat: function (array) {
+      
+
+
+  },
+ // lodash --- 04:difference
+  difference: function (array) {
+      
+
+
+  },
+// lodash --- 05:differenceBy
+  differenceBy: function (array) {
+      
+
+
+  },
+// lodash --- 06:differenceWith
+  differenceWith: function (array) {
+      
+
+
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
 
-</script>
